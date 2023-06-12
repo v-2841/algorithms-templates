135a457cfc5637e44be9a8a8fcd236042765489e
Vitaliy Pavlov <v-2841@yandex.ru>
2023-06-12 22:59:43 +0400

    12.06.23

--- python/sprint3_final/a.py
+++ python/sprint3_final/a.py
@@ -1,7 +1,21 @@
+# 88154954
 def broken_search(nums, target) -> int:
-    #  Your code
-    #  “ヽ(´▽｀)ノ”
-    pass
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if target == nums[mid]:
+            return mid
+        if nums[mid] <= nums[right]:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        else:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+    return -1
 
 
 def test():
